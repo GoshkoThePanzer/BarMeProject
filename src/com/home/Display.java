1e8c29d850f7490b4dbea07ce500d75c204b3d94
Dimo <dimo_marino@abv.bg>
2020-05-03 13:33:23 +0300

    display_constructor

--- src/com/home/Display.java
+++ src/com/home/Display.java
@@ -1,5 +1,22 @@
 package com.home;
 
+import javax.swing.*;
+
 public class Display {
-    Display() {}
+
+    JFrame frame = new JFrame();
+    int width;
+    int height;
+
+    Display(int widthSize, int heightSize, String name) {
+        this.width = widthSize;
+        this.height = heightSize;
+
+        frame.setSize(widthSize, heightSize);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setTitle(name);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 }
