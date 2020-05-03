054c9d4a0b8e1da905fb69e4259b23b33171e271
Dimo <dimo_marino@abv.bg>
2020-05-03 14:05:01 +0300

    main_methods,fields and constructor added

--- src/com/home/Page.java
+++ src/com/home/Page.java
@@ -1,4 +1,26 @@
 package com.home;
+import javax.swing.*;
 
-public class Page {
+public class Page extends JPanel {
+
+    int width;
+    int height;
+
+    Page(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void addGraphic(TextureManager texture) {
+        this.add(texture);
+    }
+
+    public void removeGraphic(TextureManager texture) {
+        this.remove(texture);
+    }
+
+    public void addButton(JButton btn) {
+        this.add(btn);
+    }
+
 }
