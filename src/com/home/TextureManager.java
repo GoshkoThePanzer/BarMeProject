25cabebf92fea551cfd484152c18bfefde8671d1
Dimo <dimo_marino@abv.bg>
2020-05-03 13:49:58 +0300

    texture_manager_constructor

--- src/com/home/TextureManager.java
+++ src/com/home/TextureManager.java
@@ -1,4 +1,22 @@
 package com.home;
 
-public class TextureManager {
+import javax.swing.*;
+
+public class TextureManager extends JComponent {
+
+    int x;
+    int y;
+    int width;
+    int height;
+    String path;
+
+    public TextureManager(int x, int y, int width, int height, String path) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.path = path;
+    }
+
+    
 }
