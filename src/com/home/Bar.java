f2bd1e3536bd8c72d215970e55a0e527dc0eeaec
Georgi Ignatov <ginchothetree@gmail.com>
2020-05-04 22:51:14 +0300

    class Bar created

---
+++ src/com/home/Bar.java
@@ -0,0 +1,52 @@
+package com.home;
+
+import java.time.LocalTime;
+
+public class Bar {
+    private String name;
+    private int location;
+    private LocalTime openingTime;
+    private LocalTime closingTime;
+
+    public Bar() {
+    }
+
+    public Bar(String name, int location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public LocalTime getOpeningTime() {
+        return openingTime;
+    }
+
+    public void setOpeningTime(LocalTime openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public LocalTime getClosingTime() {
+        return closingTime;
+    }
+
+    public void setClosingTime(LocalTime closingTime) {
+        this.closingTime = closingTime;
+    }
+}
