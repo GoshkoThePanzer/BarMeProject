48e4ef8bcb2f3d1349b7ca9920bbbd3381da30db
Georgi Ignatov <ginchothetree@gmail.com>
2020-05-04 23:44:57 +0300

    Added methods to barDB

--- src/com/home/BarDB.java
+++ src/com/home/BarDB.java
@@ -11,12 +11,30 @@ public class BarDB {
         barArray = new Bar[DEFAULT_INITIAL_SIZE];
     }
 
-    public int add(Bar addedBar){
-        if (size == barArray.length){
-            // TODO Expand Array
+    public int add(Bar addedBar) {
+        if (size == barArray.length) {
+            expandArray();
         }
         barArray[size] = addedBar;
         size++;
         return size;
+    }
+
+    public void expandArray() {
+        Bar[] newArray = new Bar[barArray.length * 2];
+        for (int i = 0; i < barArray.length; i++){
+            newArray[i] = barArray[i];
+        }
+        barArray = newArray;
+    }
+
+    public Bar getBarByIndex (int index){
+        if (index < 0) {
+            throw new IllegalArgumentException("Can't enter negative index.")
+        }
+        if (index >= size){
+            throw new IndexOutOfBoundsException("Size: " + size + "Index: " + index);
+        }
+        return barArray[index];
     }
 }
